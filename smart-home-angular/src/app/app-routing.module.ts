595391277a15e9556c8624dbcd71973f2763e5da
Maddie <madalina.khaji10@gmail.com>
2020-02-19 10:39:23 +0100

    added several components

--- smart-home-angular/src/app/app-routing.module.ts
+++ smart-home-angular/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { NetworkComponent } from './components/network/network.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'network', component: NetworkComponent },
+  {
+    path: '',
+    redirectTo: '/network',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
